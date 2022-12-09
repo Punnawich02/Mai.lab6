9154bf624c043dcdbf6c9725bf17b1d17ec86326
= <=>
2022-12-09 17:20:30 +0700

    lab6_2.cpp completed

--- lab6_2.cpp
+++ lab6_2.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
+
+
+double deg2rad(double degree){
+    double radiun = (M_PI/180)*degree ;
+    return radiun ;
+}
+
+double rad2deg(double radiun){
+    double degree = (180/M_PI)*radiun ;
+    return degree ;
+}
+
+double findXComponent(double length_A ,double length_B ,double radiun_A ,double radiun_B){
+
+    double xComponent = (cos(radiun_A)*length_A) + (cos(radiun_B)*length_B) ;
+    return xComponent ;
+}
+
+double findYComponent(double length_A ,double length_B ,double radiun_A ,double radiun_B){
+    
+    double yComponent = (sin(radiun_A)*length_A) + (sin(radiun_B)*length_B) ;
+    return yComponent ;
+}
+
+double pythagoras(double xCompo ,double yCompo){
+
+    double result_length = sqrt( pow(xCompo ,2) + pow(yCompo ,2) );
+    return result_length ;
+}
+
+void showResult(double result_length ,double result_degree){
+    cout << "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" ;
+    cout << "\nLength of the resultant vector = " << result_length ;
+    cout << "\nDirection of the resultant vector (deg) = " << result_degree ;
+    cout << "\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" ;
+}
+
 
 int main(){
     double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
@@ -14,7 +52,7 @@ int main(){
     cin >> a2;
     
     a1 = deg2rad(a1);
-    a2 = deg2rad(a2);
+    a2 = deg2rad(a2);     
     xcomp = findXComponent(l1,l2,a1,a2);
     ycomp = findYComponent(l1,l2,a1,a2);
     result_vec_length = pythagoras(xcomp,ycomp);
