f73e5d8f7ebf3f3eef0d86d32bef2c759017ed23
= <=>
2022-12-09 15:06:39 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,27 @@
 #include<iostream>
+#include <string>
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    
+    int num = 1,even = 0 ,odd = 0;
+    
+    while(num != 0){
+        
+        cout << "Enter an integer: ";    
+        cin >> num ;
+        
+        if(num%2 == 0){
+            even++ ;
+        }
+        else{
+            odd++ ;
+        }
+        
+    }
+    even-- ;
+    
+    cout << "#Even numbers = " << even ;
+    cout << "\n#Odd numbers = " << odd ;
     return 0;
 }
